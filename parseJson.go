690b3dfd627206194f1a7d839a5a0bba7e6dba65
Punith Kumar A <punitakumara623@outlook.com>
2024-06-02 19:06:42 +0530

    Added Number parsing.
    Now the json can have numbers and it will be parsed accordingly.
    It can handle negative number and fraction.

--- parseJson.go
+++ parseJson.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 //NOTE: Found JSON grammer from https://www.json.org/json-en.html, https://www.crockford.com/mckeeman.html
@@ -57,22 +58,25 @@ func valuef(ip *int, b []rune, r reflect.Value) {
 		value := stringf(ip, b)
 		if r.Kind() == reflect.String {
 			r.SetString(value)
+		} else {
+			fmt.Printf("Field  type mismatch : have %s, given %s\n", r.Kind(), "string")
 		}
 		fmt.Println("value : ", value)
 		break
-		// case 't':
-		// 	truef(ip,b)
-		// 	break
-		// case 'f':
-		// 	falsef(ip,b)
-		// 	break
+	case 't':
+		boolf(ip, b, r, "true")
+		break
+	case 'f':
+		boolf(ip, b, r, "false")
+		break
 		// case 'n':
 		// 	nullf(ip,b)
 		// 	break
 	}
-	// if isNumber(current) {
-	// 	numberf(ip,b)
-	// }
+	if isNumber(current) {
+		numberf(ip, b, r)
+	}
+	synchronize(ip, b)
 }
 
 func objectf(ip *int, b []rune, r reflect.Value) {
@@ -143,25 +147,91 @@ func stringf(ip *int, b []rune) string {
 	return string(str)
 }
 
-//
-// func numberf() { TODO: Implement the below functions
-// }
-// func truef() {
-// }
-// func falsef() {
-// }
-// func nullf() {
+/*
+boolf(ip *int, b []rune, r reflect.Value, value string)
+
+A function which is used to parse and set boolean values to the field
+It completely consumes the boolean value and points ip to the next rune.
+*/
+func boolf(ip *int, b []rune, r reflect.Value, value string) {
+	if string(b[*ip:*ip+len(value)]) == value {
+		if r.Kind() == reflect.Bool {
+			if value[0] == 't' {
+				r.SetBool(true)
+			} else {
+				r.SetBool(false)
+			}
+			*ip += len(value)
+		} else {
+			fmt.Println("Field not set for bool")
+		}
+	} else {
+		synchronize(ip, b)
+	}
+
+}
+
+func synchronize(ip *int, b []rune) {
+	for len(b) > *ip {
+		if b[*ip] == ',' || b[*ip] == ']' || b[*ip] == '}' {
+			break
+		}
+		*ip++
+	}
+}
+
+func isNumber(b rune) bool {
+	return (b >= '0' && b <= '9') || b == '-'
+}
+
+func numberf(ip *int, b []rune, r reflect.Value) {
+	isfraction := false
+
+	str := make([]rune, 0, 8)
+	if b[*ip] == '-' {
+		str = append(str, b[*ip])
+		*ip++
+	}
+	for (b[*ip] <= '9' && b[*ip] >= '0') || b[*ip] == '.' {
+		if b[*ip] == '.' {
+			isfraction = true
+		}
+		str = append(str, b[*ip])
+		*ip++
+	}
+	if isfraction {
+		i, err := strconv.ParseFloat(string(str), 32)
+		if err != nil {
+			fmt.Println("Error parsing float")
+			return
+		}
+		r.SetFloat(i)
+	} else {
+		i, err := strconv.Atoi(string(str))
+		if err != nil {
+			fmt.Println("Error parsing int")
+			return
+		}
+		r.SetInt(int64(i))
+	}
+}
+
+// func nullf() { TODO: Implement the below functions
 // }
 
 type Message struct {
-	Name innerMessage
+	Name   innerMessage
+	Number int
+	Float  float64
 }
 type innerMessage struct {
 	Name2 string
+	Flag  bool
+	Flag2 bool
 }
 
 func main() {
-	b := []rune(`{"Name":{"Name2":"Valu\"e2"}}`)
+	b := []rune(`{   "Name":{"Name2":"Valu\"e2", "Flag": true, "Flag2":false},"Number":-232,"Float":32.23}`)
 	ip := 0
 	var m Message
 	fmt.Printf("Before : %+v\n", m)
